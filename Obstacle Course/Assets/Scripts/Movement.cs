873371df074747dfb75e860005055560d6fe1450
Michael McClain <Mikemcclain14@gmail.com>
2022-10-17 12:08:48 +0200

    Video 2.18 & 2.19 finished

--- Obstacle Course/Assets/Scripts/Movement.cs
+++ Obstacle Course/Assets/Scripts/Movement.cs
@@ -9,14 +9,27 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
         transform.Translate(xValue, 0f, zValue);
     }
 }
